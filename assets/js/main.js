47a680e04e757086c6ae421fae1dd7586b94cb65
Matheus Filipe T. Chaves <matheus.02468@gmail.com>
2017-08-01 13:12:14 -0300

    Adjusted the index.html file.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
 "use strict";
 
 $("body").scrollspy({ target: "#menu" });
+
+$("#menu a").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        var hash = this.hash;
+
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+            window.location.hash = hash;
+        });
+    }
+});
