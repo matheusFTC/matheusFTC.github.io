eddf8bd6290c9c55a9109bf93cd4b588e25fd991
Matheus Filipe Teixeira Chaves <matheus.02468@gmail.com>
2017-08-05 14:38:30 -0300

    Added message sending function.

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,25 @@ $("#menu a").on("click", function (event) {
         });
     }
 });
+
+$("#buttonSend").on("click", function (event) {
+    var name = $("#inputName").val();
+    var email = $("#inputEmail").val();
+    var subject = $("#inputSubject").val();
+    var message = $("#textareaMessage").val();
+
+    if (!name || !email || !subject || !message) {
+        alert("Please enter your name, email, subject and message.");
+    } else {
+        $.post("https://postman-api.herokuapp.com", {
+            from: "no-reply@matheusftc.github.io",
+            to: "matheus.02468@gmail.com",
+            subject: "Github Page Contact",
+            text: name + " <" + email + ">: (" + subject + "): " + message
+        }).done(function (data) {
+            alert("Message sent successfully.");
+        }).fail(function () {
+            alert("Sorry, we could not send your message.");
+        });
+    }
+});
