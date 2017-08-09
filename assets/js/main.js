ea3d699e3164c6b096968ba348fc90b82f615831
Matheus Filipe T. Chaves <matheus.02468@gmail.com>
2017-08-09 11:41:09 -0300

    Adjusted the main.js file.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 $("body").scrollspy({ target: "#menu" });
+$("div.alert").hide();
 
 $("#menu a").on("click", function (event) {
     if (this.hash !== "") {
@@ -16,24 +17,48 @@ $("#menu a").on("click", function (event) {
     }
 });
 
-$("#buttonSend").on("click", function (event) {
+function alert(text, style) {
+    $("div.alert")
+        .removeClass("alert-success")
+        .removeClass("alert-danger")
+        .addClass(style)
+        .text(text)
+        .show();
+};
+
+function success(text) {
+    alert(text, "alert-success");
+};
+
+function error(text) {
+    alert(text, "alert-danger");
+};
+
+$("div#contact form button").on("click", function (event) {
     var name = $("#inputName").val();
     var email = $("#inputEmail").val();
     var subject = $("#inputSubject").val();
     var message = $("#textareaMessage").val();
 
-    if (!name || !email || !subject || !message) {
-        alert("Please enter your name, email, subject and message.");
-    } else {
+    if (name && email && subject && message) {
+        $("div#contact form button").prop("disabled", true);
+
         $.post("https://postman-api.herokuapp.com", {
             from: "no-reply@matheusftc.github.io",
             to: "matheus.02468@gmail.com",
             subject: "Github Page Contact",
             text: name + " <" + email + ">: (" + subject + "): " + message
         }).done(function (data) {
-            alert("Message sent successfully.");
+            success("Message sent successfully.");
         }).fail(function () {
-            alert("Sorry, we could not send your message.");
+            error("Sorry, we could not send your message.");
+        }).always(function () {
+            $("div#contact form input.form-control").val(null);
+            $("div#contact form textarea.form-control").val(null);
+            
+            $("div#contact form button").prop("disabled", false);
         });
+    } else {
+        error("Please enter your name, email, subject and message.");
     }
 });
